caae7ba94298fa43390b7515ed2cd88f121a04ae
Dalton <cdaltonrowe@gmail.com>
2024-10-14 09:18:56 +0200

    add some logging

--- wled-control/src/index.js
+++ wled-control/src/index.js
@@ -4,6 +4,13 @@ require("dotenv").config();
 
 const { power, getState, getNodes } = require('./api')
 
+function log() {
+  if (process.env.LOG === '1') {
+    console.log(arguments);
+
+  }
+}
+
 const stateTarget = {
   nightTime: false,
   host: {},
@@ -26,7 +33,7 @@ const updateNighttime = () => {
 updateNighttime();
 
 let last = Date.now()
-let int = 15_000;
+let int = 60_000;
 let firstRun = true
 let running = false;
 
@@ -38,6 +45,8 @@ setInterval(async () => {
   updateNighttime();
 
   if (firstRun) {
+    log('First run, getting state.');
+    log(`Host Node: ${process.env.HOST_NODE}`);
     firstRun = false
     state.host = await getState()
     state.nodes = await getNodes()
@@ -49,6 +58,8 @@ setInterval(async () => {
     const shouldBeOn = state.nightTime
     const isOn = state.host.on
     const needsPowerChanged = shouldBeOn != isOn
+
+    log(`shouldBeOn: ${shouldBeOn} | isOn: ${isOn} | needsPowerChanged: ${needsPowerChanged}`);
 
     if (needsPowerChanged) {
       const updatePromise = new Promise(async (resolve, _reject) => {
